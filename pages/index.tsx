c8a3bf209c635824929d390f2b0819d0c3ee1e13
Taylor Smith <tsmith@tsmithcreative.com>
2022-09-09 09:54:16 -0500

    Initial commit with hacked together Next.js starter

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Paperframe</title>
+      </Head>
+      <header>
+        <h1>Paperframe</h1>
+      </header>
+    </>
+  );
+}
