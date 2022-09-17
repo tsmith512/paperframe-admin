3ac0cb0159b5ef718aca9cef17c017db795758d0
Taylor Smith <tsmith@tsmithcreative.com>
2022-09-17 18:25:17 -0500

    Order isn't an image property anymore, remove it from the carousel and just count

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -13,20 +13,22 @@ interface CarouselProps {
 export const Carousel = (props: CarouselProps) => {
   return (
     <div className={style.carousel}>
-      {props.images.map((image) => (
+      {props.images.map((image, index) => (
         <div
           key={image.id}
           className={style.slide}
           data-active={props.active === image.id ? true : null}
         >
-          <div className={style.order}>{image.order}</div>
+          <div className={style.order}>{index}</div>
           <div className={style.image}>
             <img
               src={`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/image/${image.id}`}
               alt={image.title}
             />
           </div>
-          <div className={style.title}>{image.title}</div>
+          <div className={style.title}>
+            <span>{image.id}</span> {image.title}
+          </div>
           {props.authenticated && (
             <div className={style.actions}>
               <button>Display</button>
